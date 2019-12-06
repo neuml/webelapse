3481ec703cb007783a19f508ac45663bfc119571
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2019-12-05 19:59:57 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,71 @@
+webelapse: Generate time-lapse video for a website
+======
+
+webelapse is a Node.js program that can be used to generate time-lapse video of a website. Possible use cases are helping to review/test/demo web application functionality before releasing to production or building screencasts of frequently updated websites.
+
+webelapse has the following features:
+ - Generate time-lapse video for a given website/URL
+ - Account for duplicate/near duplicate frames and remove them from the output
+ - Exponentially back off the next scheduled execution time if the content is not changing up to a max timeout interval
+ - Generate video after times of frequent change and a return to static content
+
+## Installation
+webelapse can be installed via npm
+
+    npm install -g webelapse
+
+This package assumes [ffmpeg](https://www.ffmpeg.org/) is installed and available on the user's PATH. ffmpeg is required to generate videos.
+
+## Examples
+Take a screenshot of a website on localhost, store files in directory named output and exit.
+
+    webelapse -u http://localhost:8080 -o output
+
+Take a screenshot of a website on localhost every minute (60 seconds). Duplicate frames discarded. No video built.
+
+    webelapse -u http://localhost:8080 -o output -s 60
+
+Record a stream of a website on localhost, taking screenshots every minute (60 seconds). Build video every 30 frames.
+ 
+    webelapse -u http://localhost:8080 -o output -s 60 -f 30
+
+Record a stream of a website on localhost, taking screenshots every minute (60 seconds). Exponentially back off static content, increasing the interval for static content up to 1800 seconds (30 minutes) between frames. Video built dynamically once max backoff time reached.
+
+    webelapse -u http://localhost:8080 -o output -s 60 -m 1800
+
+Same as previous but will consider frames a duplicate if its hash is within an edit distance of 2. Default is to require an exact image hash, which will resolve to the same hash for near duplicate images.
+
+    webelapse -u http://localhost:8080 -o output/desktop -d 2 -s 60 -m 1800
+
+Show all options.
+
+    webelapse --help
+
+## Other installation methods
+
+If you do not want to install the package globally, it can be installed in a local directory and executed as (instead of webelapse):
+
+    npm install webelapse
+    node node_modules/webelapse/record.js
+
+Alternatively, you can use Git to clone the repository from GitHub:
+
+    git clone https://github.com/davidmezzetti/webelapse.git
+    cd webelapse
+    npm update
+    node record.js
+
+## Background
+
+Screenshots and screencasts of a website are common and there are a number of packages out there built on either Selenium or Puppeteer. This package adds duplicate and near duplicate detection, to only record dynamic activity. This package is designed to work with websites that frequently update without user action, such as a live dashboard. Duplicate detection eliminates extremely long videos with nothing changing, while allowing you to see what is changing fast.
+
+webelapse uses [Puppeteer](https://www.npmjs.com/package/puppeteer) for web automation and perceptual hashing via [imghash](https://www.npmjs.com/package/imghash) for near duplicate detection. Block mean perceptual hashing is designed to generate similar hashes for similar images. If a webpage has a single line of text updated, such as a timestamp, those frames can ignored. [ffmpeg](https://www.ffmpeg.org/) is used for encoding output videos and it is required to be installed on the user's PATH.
+
+## Security
+
+webelapse is built on Puppeteer, which is built on the Chromium web stack. While Chromium has many security measures built in, you should still trust the source of URLs browsed as they will be loaded on your machine locally to render screenshots.
+
+webelapse has features such as exponential backoff to increase the scheduled run frequency when content is static. Common sense should be used to only schedule the run frequency to what is necessary.
+
+## License
+MIT
---
+++ record.js
@@ -0,0 +1,211 @@
+#!/usr/bin/env node
+
+/**
+ *  Node.js script that uses puppeteer to generate time lapse video for a web page. It has the following features:
+ *
+ *  - Generate time-lapse video for a given website/URL
+ *  - Account for duplicate/near duplicate frames and remove them from the output
+ *  - Exponentially back off the next scheduled execution time if the content is not changing up to a max timeout interval
+ *  - Generate video after times of frequent change and a return to static content
+ */
+
+const argv = require("commander");
+const child = require("child_process");
+const fs = require("fs");
+const imghash = require("imghash");
+const leven = require("leven");
+const moment = require("moment");
+const path = require("path");
+const puppeteer = require("puppeteer");
+const util = require("util");
+
+// Create promise based versions of functions to work in async mode
+const exec = util.promisify(child.exec);
+const readdir = util.promisify(fs.readdir);
+const writeFile = util.promisify(fs.writeFile);
+const unlink = util.promisify(fs.unlink);
+
+// Command line argument parsing
+argv.requiredOption("-o --output <output directory>", "Output directory (required)");
+argv.requiredOption("-u --url <url>", "URL to record (required)");
+argv.option("-b --bits <bits>", "Hash bits per row. Defaults to 12. Larger sizes are more sensitive to small image changes", parseInt);
+argv.option("-d --distance <distance>", "Edit distance between hashes to be considered a duplicate. Defaults to 0.", parseInt);
+argv.option("-e --encoding <output encoding", "Output file encoding. Defaults to mp4");
+argv.option("-f --frames <count>", "Number of frames to use for each video. If empty, will only generate video between active times.", parseInt);
+argv.option("-m --max <seconds>", "Maximum time to wait to schedule in seconds. Defaults to 1 day max.", parseInt);
+argv.option("-s --schedule <seconds>", "Generate file every S seconds, with exponential backoff on static content", parseInt);
+argv.option("-w --width <window width>", "Browser view width", parseInt);
+argv.option("-t --height <window height>", "Browser view height", parseInt);
+
+argv.parse(process.argv);
+
+let hash = null;
+let frames = [];
+let duplicates = 0;
+
+/**
+ * Initializes a new run. Attempts to recover frames from a previous partial run.
+ */
+async function init() {
+  // Read output directory for frames from previous runs
+  let files = await readdir(argv.output)
+
+  if (files && files.length > 0) {
+    files = files.filter(f => /webelapse\-\d+\.png/.test(f));
+
+    for (let x = 0; x < files.length; x++) {
+      frames.push(path.join(argv.output, files[x]));
+    }
+
+    // Ensure frames are sorted
+    frames.sort();
+  }
+
+  if (frames.length > 0) {
+    // Get hash from last frame
+    hash = await imghash.hash(frames[frames.length - 1], argv.bits || 12);
+
+    log(util.format("Recovered %d frames from previous run", frames.length));
+    log("Recovered hash " + hash);
+  }
+}
+
+/**
+ * Generates a single frame. This method only keeps the new frame if it has a 
+ * different perceptual hash from the previous frame.
+ */
+async function run() {
+  // Generate output path
+  const output = path.join(argv.output, "webelapse-" + new Date().getTime() + ".png");
+
+  // Get frame data
+  let data = await frame(output);
+
+  // Build image hash to detect difference from previous frame
+  let newhash = await imghash.hash(data, argv.bits || 12);
+  let distance = argv.distance || 0;
+
+  // Check if the frame hash is a duplicate or near duplicate of the previous frame hash
+  if (!hash || leven(hash, newhash) > distance) {
+    // Write data to file
+    await writeFile(output, data);
+
+    log(util.format("Wrote frame: %s, hash: %s", output, newhash));
+    frames.push(output);
+    duplicates = 0;
+  }
+  else {
+    log(util.format("Duplicate hash %s", newhash));
+    duplicates++;
+  }
+
+  // Store new hash
+  hash = newhash;
+
+  // Schedule next execution
+  schedule();
+}
+
+/**
+ * Logs a message to the console.
+ * 
+ * @param message message to log
+ */
+function log(message) {
+  const timestamp = moment().format("YYYY-MM-DD HH:mm:ss");
+  console.log(timestamp + " - " + message)
+}
+
+/**
+ * Schedules the next execution run, if scheduling is enabled.
+ */
+async function schedule() {
+  if (argv.schedule) {
+    // Default timeout in seconds
+    let timeout = argv.schedule * 1000;
+
+    // Default max timeout is 1 day if not provided
+    let max = argv.max ? argv.max * 1000 : 1000 * 60 * 60 * 24;
+
+    // Exponential backoff up to maximum time
+    timeout = Math.min(duplicates > 0 ? timeout * Math.pow(2, duplicates) : timeout, max);
+
+    // Build video if activity has went from dynamic to static or the number of frames has elapsed
+    if ((frames.length >= 10 && timeout >= max) || (argv.frames && frames.length >= argv.frames)) {
+      await video();
+
+      // Reset parameters
+      hash = null;
+      frames = [];
+      duplicates = 0;
+    }
+
+    setTimeout(run, timeout);
+  }
+}
+
+/**
+ * Opens puppeteer and generates a screenshot. Stores content in output file.
+ * 
+ * @param output output file
+ * @returns frame data as Buffer
+ */
+async function frame(output) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  if (argv.width && argv.height) {
+    await page.setViewport({width: argv.width, height: argv.height})
+  }
+  else {
+    await page.setViewport({width: 960, height: 720})
+  }
+
+  // Wait for page to render before taking the screenshot
+  await page.goto(argv.url, { waitUntil: "networkidle0", timeout: 60000});
+
+  // Get screenshot as image buffer, defaults to PNG format
+  let data = await page.screenshot();
+
+  await page.close();
+  await browser.close();
+
+  return data;
+}
+
+/**
+ * Generates a video using output frames and ffmpeg. Image frames are deleted after
+ * the video is generated. This method assumes ffmpeg is executable from the default PATH.
+ */
+async function video() {
+  let command = "ffmpeg -framerate 0.5 -pattern_type glob -i '%s' \
+                -vf 'mpdecimate,setpts=N/FRAME_RATE/TB,pad=ceil(iw/2)*2:ceil(ih/2)*2' \
+                -pix_fmt yuv420p %s";
+ 
+  let files = path.join(argv.output, "*.png");
+  let encoding = argv.encoding || "mp4";
+  let output = path.join(argv.output, moment().format("YYYY-MM-DD_HHmmss") + "." + encoding);
+  command = util.format(command, files, output);
+
+  // Execute ffmpeg conversion
+  await exec(command).then(async () => {
+    // Remove in progress frame files
+    for (let x = 0; x < frames.length; x++) {
+      await unlink(frames[x])
+    }
+
+    log(util.format("Wrote video %s and reset frames", output))
+  })
+}
+
+async function execute() {
+  try {
+    // Initialize and run process
+    await init();
+    run();
+  }
+  catch (error) {
+    log("Execution FAILED - " + error);
+  }
+}
+
+execute();
